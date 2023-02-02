578b3f008ed52c808c8c67345db5cb4a6ac264ff
EAgnese <agneseeri@gmail.com>
2023-02-02 16:14:17 +0100

    suppression des routes précedement suppr

--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,7 @@
 module.exports = (app) =>{
     const benevolesRouter = require('./benevoles_route');
     const jeuxRouter = require('./jeux_route');
-    const zonesRouter = require('./zones_route');
-    const jeuTypesRouter = require('./types_jeu_route');
-    const affectationsRouter = require('./affectations_route');
 
     app.use('/benevoles', benevolesRouter);
     app.use('/jeux', jeuxRouter);
-    app.use('/zones', zonesRouter);
-    app.use('/types_jeu', jeuTypesRouter);
-    app.use('/affectationss', affectationsRouter);
 }
