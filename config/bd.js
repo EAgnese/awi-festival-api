bfa4469e26f3e59556578278f155a47b5dd6e7a5
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-03-14 18:23:52 +0100

    deploy8

--- config/bd.js
+++ config/bd.js
@@ -1,7 +1,7 @@
 let mysql = require('mysql');
 
 let connection = mysql.createConnection({
-  host     : 'http://awi-festival-api.cluster-ig4.igpolytech.fr/',
+  host     : 'awi-festival-api.cluster-ig4.igpolytech.fr',
   user     : 'mysql',
   password : 'd9600d42cb844e52',
   database : 'awi_festival',
