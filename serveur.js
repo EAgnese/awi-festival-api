1ca781acab07d949b89d7c8e1db0cb5d51382643
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-03-14 10:46:11 +0100

    test deploy2

--- serveur.js
+++ serveur.js
@@ -19,7 +19,7 @@ var routes = require("./routes/route");
 routes(app) //envoi de l'app 
 
 app.use((req,res,next) =>{
-  res.status(404).send('page not found')
+  res.status(404).send('page not found ALSO')
 })
 
 const PORT = process.env.PORT || 3000
