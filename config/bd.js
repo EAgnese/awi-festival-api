2f68573603966c31ce92759700440f09a431f7cb
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-03-14 20:27:58 +0100

    deploy13

--- config/bd.js
+++ config/bd.js
@@ -2,6 +2,8 @@ let mysql = require('mysql');
 require("dotenv").config()
 const url = require('url');
 
+/*
+
 console.log(process.env.DATABASE_URL)
 const dbUrl = process.env.DATABASE_URL; // assuming this is where you have stored the DSN
 const params = url.parse(dbUrl);
@@ -14,7 +16,7 @@ const connection = mysql.createConnection({
   password: params.auth.split(':')[1],
   database: params.pathname.slice(1)
 });
-
+*/
 /*
 
 let connection = mysql.createConnection({
@@ -25,14 +27,14 @@ let connection = mysql.createConnection({
   //port     : '3306',
   connectTimeout: 1000000,
 });*/
-/*
+
 let connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'simplify_awi',
   password : 'Awi_festival02',
   database : 'awi_festival',
   port     : '3306',
-});*/
+});
 connection.connect(function(err) {
   if (err) {
       console.log('Connexion error ' + err + ' ' + err.code);
